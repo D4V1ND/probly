91b6ccbad6b374cfca6670de3e2185fe567ab768
Domi <238303207+dsme42@users.noreply.github.com>
2025-11-26 12:17:32 +0100

    implemented wrapper for isotonic regression with torch

---
+++ src/probly/calibration/isotonic_regression/torch.py
@@ -0,0 +1,117 @@
+"""Torch Implementations for Isotonic Regression."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import numpy as np
+from sklearn.isotonic import IsotonicRegression
+from torch import Tensor, cat, device as TorchDevice, nn, no_grad, sigmoid, softmax, tensor, unique
+
+if TYPE_CHECKING:
+    from torch.utils.data import DataLoader
+
+
+class IsotonicRegressionCalibrator:
+    """Class for the isotonic regression calibration."""
+
+    def __init__(self, base_model: nn.Module, device: TorchDevice) -> None:
+        """Set up the wrapper.
+
+        Args:
+            base_model: The base model to calibrate
+            device: The device that torch uses for the model
+
+        """
+        self.model = base_model
+        self.device = device
+        self.calibrator = None
+
+    def fit(self, calibration_set: DataLoader) -> None:
+        """Fit the regression function to the model outputs.
+
+        Args:
+            calibration_set: The set that should be used for the calibration
+
+        """
+        # get logits for calibration data
+        logits_and_labels = self._extract_logits_and_labels(calibration_set)
+        logits_cal = logits_and_labels[0]
+        labels_cal = logits_and_labels[1]
+
+        # Identify number of classes and learn regression function
+        num_classes = len(unique(labels_cal))
+
+        if(num_classes <= 2):
+            scores = self._get_binary_scores(logits_cal)
+            probabilities = sigmoid(scores).cpu()
+            iso_reg = IsotonicRegression(y_min=0.0, y_max=1.0, out_of_bounds="clip")
+            iso_reg.fit(probabilities.numpy(), labels_cal.numpy())
+            self.calibrator = iso_reg
+
+        else:
+            probabilities = softmax(logits_cal, dim=1).cpu().numpy()
+            self.calibrator = []
+
+            for c in range(num_classes):
+                iso_reg = IsotonicRegression(y_min=0.0, y_max=1.0, out_of_bounds="clip")
+                iso_reg.fit(probabilities[:, c], (labels_cal == c).numpy())
+                self.calibrator.append(iso_reg)
+
+    def predict(self, x: Tensor) -> Tensor:
+        """Make calibrated predictions on the input x.
+
+        Args:
+            x: The input for the model to make predictions on
+
+        Returns:
+            calibrated_probs: The calibrated probabilities for the prediction
+
+        """
+        self.model.eval()
+
+        with no_grad():
+            x = x.to(self.device)
+            logits = self.model(x)
+
+            # Case Multiclass model
+            if(isinstance(self.calibrator, list)):
+                probabilities = softmax(logits, dim=1).cpu().numpy()
+                calibrated_probs = np.vstack([
+                    iso.predict(probabilities[:,c]) for c, iso in enumerate(self.calibrator)]).T
+
+                calibrated_probs = calibrated_probs / calibrated_probs.sum(axis=1, keepdims=True)
+                return tensor(calibrated_probs, device=self.device)
+
+            # Case Binary Model
+            scores = self._get_binary_scores(logits)
+            probabilities = sigmoid(scores).cpu().numpy()
+            calibrated_probs = self.calibrator.predict(probabilities)
+            return tensor(calibrated_probs, device=self.device)
+
+    def _extract_logits_and_labels(self, dataset: DataLoader) -> tuple[Tensor]:
+        """Returns the logits and labels for a dataset as a tuple (logits, labels)."""
+        self.model.eval()
+
+        logits = []
+        labels = []
+
+        with no_grad():
+            for x, y in dataset:
+                inpt = x.to(self.device)
+                output = self.model(inpt)
+                logits.append(output.cpu())
+                labels.append(y.cpu())
+
+        return (cat(logits), cat(labels))
+
+    def _get_binary_scores(self, logits: Tensor) -> Tensor:
+            if(logits.ndim == 1):
+                return logits
+            if(logits.shape[1] == 1):
+                return logits.squeeze()
+            if(logits.shape[1] == 2):
+                return logits[:, 1]
+
+            value_error_str = f"Binary Model should output shape (N,2), (N,1), (N,), not {tuple(logits.shape)}"
+            raise ValueError(value_error_str)
