6c1aaf68e3fa126834d63789df42ac837c656ae9
Shoha <shoha28kyu@gmail.com>
2025-11-27 17:35:58 +0100

    First implementation of the Calibrator Abstract Class and removed unnecessary text in histogram binning common

---
+++ src/probly/calibration/template.py
@@ -0,0 +1,34 @@
+"""Basic Template for Calibration Methods with Torch and Flax."""
+
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, Self
+
+if TYPE_CHECKING:
+    from torch import Tensor, TorchDevice, nn
+    from torch.utils.data import DataLoader
+
+
+class CalibratorBaseTorch(ABC):
+    """Abstract base class for calibrators."""
+
+    def __init__(self, base_model: nn.Module, device: TorchDevice) -> None:
+        """Create a calibrator.
+
+        Args:
+            base_model: The base model whose outputs are to be calibrated.
+            device: Torch device or a device string (e.g. 'cpu', 'cuda:0').
+        """
+        self.model = base_model
+        self.device = device
+
+    @abstractmethod
+    def fit(self, calibration_set: DataLoader) -> Self:
+        """Fit calibrator from calibration_set (DataLoader-like)."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def predict(self, x: Tensor) -> Tensor:
+        """Return calibrated probabilities for input x."""
+        raise NotImplementedError
